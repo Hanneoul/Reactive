a274adafe251f74f29a430620f52b0df44cecff0
elflee77 <elflee77@gmail.com>
2024-04-29 14:11:49 +0900

    8주차

--- Lecture/Week08/main.js
+++ Lecture/Week08/main.js
@@ -3,9 +3,11 @@ var canvas = document.getElementById("GameScreenCanvas");
 var ctx = canvas.getContext("2d");
 
 class Circle{
-    constructor(posX, posY, color, size, moveDirX, moveDirY, speed){
+    constructor(posX, posY, rot, rotSpeed, size, color, moveDirX, moveDirY, speed){
         this.position_X = posX;
         this.position_Y = posY;
+        this.rotation_Y = rot;
+        this.rotation_Speed = rotSpeed;
         this.color = color;
         this.size = size;
 
@@ -16,39 +18,43 @@ class Circle{
 
     update(){
 
-        this.position_X += (this.moveDirectionX * this.speed);
-        this.position_Y += (this.moveDirectionY * this.speed);
+        this.position_X += (this.moveDirectionX * this.moveSpeed);
+        this.position_Y += (this.moveDirectionY * this.moveSpeed);
+
+        this.rotation_Y += this.rotation_Speed;
 
     }
 
     draw(){
         ctx.beginPath();
     
-        for(var i=0;i<360;i++){
+        for(var i=this.rotation_Y;i<=360+this.rotation_Y;i+=72){
             ctx.lineTo((Math.cos(Math.PI/180*i)) * this.size + this.position_X, (Math.sin(Math.PI/180*i)) * this.size + this.position_Y);
         }
 
-        
-        
-
         ctx.fillStyle = this.color;
         ctx.fill();
+        ctx.stroke();
 
         ctx.closePath();
 
     }
 }
 
-var c = new Circle(50.0,50.0,"orange", 50.0, 1.0,1.0,0.0);
+var c = new Circle(0.0, 0.0, -90.0, 1.0, 50.0, "orange", 1.0, 1.0, 0.0);
 
 function draw()
 {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    c.update();
+    
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
 
-    //c.update();
-    c.position_X ++;
-    
     c.draw();
-    
+    ctx.restore();
+
     requestAnimationFrame(draw);
 }
 
